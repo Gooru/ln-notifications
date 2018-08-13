77385d33031b76cedc0b65d4042cf382098e510c
am-gooru <ashish@gooru.org>
2018-08-13 14:19:10 +0530

    Provide master tables for notification types lookup.

--- sql/notifications-v0.sql
+++ sql/notifications-v0.sql
@@ -1,5 +1,28 @@
+-- drop table teacher_notifications_master
+-- drop table student_notifications_master
 -- drop table teacher_notifications
 -- drop table student_notifications
+
+create table teacher_notifications_master (
+    name text PRIMARY KEY,
+    description text,
+    is_actionable boolean NOT NULL DEFAULT true,
+    created_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
+    updated_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL
+);
+
+COMMENT on table teacher_notifications_master IS 'Master table to store notifications types and its metadata for teachers';
+
+create table student_notifications_master (
+    name text PRIMARY KEY,
+    description text,
+    is_actionable boolean NOT NULL DEFAULT true,
+    created_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
+    updated_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL
+);
+
+COMMENT on table student_notifications_master IS 'Master table to store notifications types and its metadata for students';
+
 
 create table teacher_notifications (
     id bigserial NOT NULL,
@@ -8,19 +31,20 @@ create table teacher_notifications (
     ctx_course_id uuid NOT NULL,
     ctx_unit_id uuid NOT NULL,
     ctx_lesson_id uuid NOT NULL,
-    ctx_collection_id uuid NOT NULL,
+    ctx_collection_id uuid,
     current_item_id uuid NOT NULL,
     current_item_type text NOT NULL CHECK (current_item_type::text = ANY (ARRAY['collection'::text, 'assessment'::text,
         'collection-external'::text, 'assessment-external'::text ])),
     current_item_title text NOT NULL,
-    notification_type text NOT NULL CHECK (ctx_path_type::text = ANY (ARRAY['grading.pending'::text])),
+    notification_type text NOT NULL,
     ctx_path_id bigint,
     ctx_path_type text CHECK (ctx_path_type::text = ANY (ARRAY['system'::text, 'teacher'::text, 'route0'::text])),
     occurrence int NOT NULL DEFAULT 0,
     users text[] NOT NULL,
     created_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
     updated_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
-    CONSTRAINT tn_pkey PRIMARY KEY (id)
+    CONSTRAINT tn_pkey PRIMARY KEY (id),
+    FOREIGN KEY (notification_type) references teacher_notifications_master (name)
 );
 
 ALTER TABLE teacher_notifications OWNER TO nucleus;
@@ -31,12 +55,22 @@ ALTER TABLE teacher_notifications ADD CONSTRAINT pi_pt_chk CHECK ((ctx_path_id i
 CREATE UNIQUE INDEX tn_cculcnitpp_unq_idx
     ON teacher_notifications (ctx_class_id, ctx_course_id, ctx_unit_id, ctx_lesson_id, ctx_collection_id,
     current_item_id, current_item_type, notification_type, ctx_path_id, ctx_path_type)
-    where ctx_path_id is not null and ctx_path_type is not null;
+    where ctx_path_id is not null and ctx_path_type is not null and ctx_collection_id is not null;
 
 CREATE UNIQUE INDEX tn_cculcitn_unq_idx
     ON teacher_notifications (ctx_class_id, ctx_course_id, ctx_unit_id, ctx_lesson_id, ctx_collection_id,
     current_item_id, current_item_type, notification_type)
-    where ctx_path_id is null and ctx_path_type is null;
+    where ctx_path_id is null and ctx_path_type is null and ctx_collection_id is not null;
+
+CREATE UNIQUE INDEX tn_cculcnitpp1_unq_idx
+    ON teacher_notifications (ctx_class_id, ctx_course_id, ctx_unit_id, ctx_lesson_id,
+    current_item_id, current_item_type, notification_type, ctx_path_id, ctx_path_type)
+    where ctx_path_id is not null and ctx_path_type is not null and ctx_collection_id is null;
+
+CREATE UNIQUE INDEX tn_cculcitn1_unq_idx
+    ON teacher_notifications (ctx_class_id, ctx_course_id, ctx_unit_id, ctx_lesson_id,
+    current_item_id, current_item_type, notification_type)
+    where ctx_path_id is null and ctx_path_type is null and ctx_collection_id is null;
 
 CREATE INDEX tn_ua_idx ON teacher_notifications USING btree (updated_at);
 
@@ -58,13 +92,13 @@ create table student_notifications (
     current_item_type text NOT NULL CHECK (current_item_type::text = ANY (ARRAY['collection'::text, 'assessment'::text,
         'collection-external'::text, 'assessment-external'::text ])),
     current_item_title text NOT NULL,
-    notification_type text NOT NULL CHECK (ctx_path_type::text = ANY (ARRAY['grading.pending'::text,
-        'teacher.suggestion'::text])) ,
+    notification_type text NOT NULL,
     ctx_path_id bigint,
     ctx_path_type text CHECK (ctx_path_type::text = ANY (ARRAY['system'::text, 'student'::text, 'route0'::text])),
     created_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
     updated_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
-    CONSTRAINT sn_pkey PRIMARY KEY (id)
+    CONSTRAINT sn_pkey PRIMARY KEY (id),
+    FOREIGN KEY (notification_type) references student_notifications_master(name)
 );
 
 ALTER TABLE student_notifications OWNER TO nucleus;
@@ -75,14 +109,34 @@ ALTER TABLE student_notifications ADD CONSTRAINT pi_pt_chk CHECK ((ctx_path_id i
 CREATE UNIQUE INDEX sn_ucculcppit_unq_idx
     ON student_notifications (ctx_user_id, ctx_class_id, ctx_course_id, ctx_unit_id, ctx_lesson_id, ctx_collection_id,
     current_item_id, current_item_type, ctx_path_id, ctx_path_type)
-    where ctx_path_id is not null and ctx_path_type is not null;
+    where ctx_path_id is not null and ctx_path_type is not null and ctx_collection_id is not null;
 
 CREATE UNIQUE INDEX sn_ucculcit_unq_idx
     ON student_notifications (ctx_user_id, ctx_class_id, ctx_course_id, ctx_unit_id, ctx_lesson_id, ctx_collection_id,
     current_item_id, current_item_type)
-    where ctx_path_id is null and ctx_path_type is null;
+    where ctx_path_id is null and ctx_path_type is null and ctx_collection_id is not null;
+
+CREATE UNIQUE INDEX sn_ucculcppit1_unq_idx
+    ON student_notifications (ctx_user_id, ctx_class_id, ctx_course_id, ctx_unit_id, ctx_lesson_id,
+    current_item_id, current_item_type, ctx_path_id, ctx_path_type)
+    where ctx_path_id is not null and ctx_path_type is not null and ctx_collection_id is null;
+
+CREATE UNIQUE INDEX sn_ucculcit1_unq_idx
+    ON student_notifications (ctx_user_id, ctx_class_id, ctx_course_id, ctx_unit_id, ctx_lesson_id,
+    current_item_id, current_item_type)
+    where ctx_path_id is null and ctx_path_type is null and ctx_collection_id is null;
 
 CREATE INDEX sn_ua_idx ON student_notifications USING btree (updated_at);
 
 COMMENT on table student_notifications IS 'Store the student notifications';
+
+-- Data insertions for master table
+insert into teacher_notifications_master values
+    ('student.self.report', null, false, now(), now()),
+    ('student.gradable.submission', null, true, now(), now());
+
+insert into student_notifications_master values
+    ('teacher.override', null, false, now(), now()),
+    ('teacher.grading.complete', null, false, now(), now()),
+    ('teacher.suggestion', null, true, now(), now());
 
